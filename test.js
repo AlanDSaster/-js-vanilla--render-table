6ed8711bc968c165f07c52b90dd98b57ccc60844
AlanDSaster <AlanDSaster@Outlook.com>
2022-11-02 11:41:34 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
 const records = [
-  { itemcode : 'X1234', vendor : 'exampleVendor', catalognumber : '1234' },
+	{ itemcode : 'X1234', vendor : 'exampleVendor', catalognumber : '1234' },
 	{ itemcode : 'X543', vendor : 'anotherVendor', catalognumber : '543' }
 ];
 
 function test() {
-  let nodeIdentification = {
-	  ids : [ 'test' ]
-	}
-	let headers = [ 'pemsnumber', 'vendor', 'catalognumber', 'producttype', 'productcategory', 'productclass' ];
+	let nodeIdentification = { ids : [ 'test' ] };
+	let headers = [ 'itemcode', 'vendor', 'catalognumber' ];
 	let tableData = recordsToElementsData(records, headers);
 	console.log('rendering records');
 	renderRecords(nodeIdentification, tableData);
